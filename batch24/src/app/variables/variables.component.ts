137f5d1df23fba5a2360774af11ec0579f53baa1
Mahesh Perla <maheshp212@gmail.com>
2018-11-22 10:28:45 +0530

    uifixe

--- batch24/src/app/variables/variables.component.ts
+++ batch24/src/app/variables/variables.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-variables',
   templateUrl: './variables.component.html',
   styleUrls: ['./variables.component.scss']
 })
-export class VariablesComponent implements OnInit {
-
+export class VariablesComponent  {
+	fname:string = 'hello';
+	age = 34;
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
