495a476135c43e4d52b555dce762a82ddb563950
Mahesh Perla <maheshp212@gmail.com>
2019-04-15 09:07:29 +0530

    ui added

--- venky/src/app/app-routing.module.ts
+++ venky/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { IntroComponent } from './intro/intro.component';
+import { VariablesComponent } from './variables/variables.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'intro', component:IntroComponent},
+  {path:'variables', component:VariablesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- venky/src/app/app.module.ts
+++ venky/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { IntroComponent } from './intro/intro.component';
+import { VariablesComponent } from './variables/variables.component';
 
 @NgModule({
   declarations: [ // component , directive , pipe
-    AppComponent
+    AppComponent, HomeComponent, IntroComponent, VariablesComponent
   ],
   imports: [ // modules
     BrowserModule,
